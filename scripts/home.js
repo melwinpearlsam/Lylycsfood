7038acb57b5e5b3a7e7f5ff97fd470ee7bfdd39f
unknown <melwin280@gmail.com>
2021-05-25 13:56:17 +0530

    progress

--- scripts/home.js
+++ scripts/home.js
@@ -36,31 +36,31 @@ async function getRestaurants() {
 // Components
 const restaurantCard = (r) => {
 	return `<div id="restaurant-list">
-    <div class="restaurant-card">
+    <div class=" restaurant-card" style="border-radius: 15px !important;box-shadow: 1px 1px 10px 1px rgb(179, 179, 179);">
         <!-- Image -->
-        <div style="width: 90vw" class="restaurant-card-image">
+        <div style="width: 90vw;" class="restaurant-card-image">
             <img
-                style="object-fit: fill"
+                style="object-fit: fill; "
                 width="100%"
-                height="150px"
-                src="https://lylycsfood.herokuapp.com/${r.displayImage}"
+                height="170px"
+                src="/assets/default-restaurant.png"
                 alt="restaurant"
             />
         </div>
-        <div style="padding: 8px">
+        <div style="padding: 8px; display: flex; justify-content: space-between;">
             <!-- Name -->
-            <h3 style="color: grey">${r.displayName}</h3>
+            <h3 style="color: grey; margin-left: 10px;">Kunju</h3>
             <a style="text-decoration: none;color:black" href="./restaurantDetails.html?id=${r._id}">
-                <div class="info">
+                <div class="info" style="padding: 5px 10px; border-radius: 7px; background-color: black;color: white;">
                 <i class="fas fa-info"></i>
-                <div style="width:8px"></div>
+                <div style="width:8px; ;"></div>
                 info
             </div>
             </a>
         </div>
         <!-- Edit -->
         <a style="text-decoration: none;" href="./restaurant.html?id=${r._id}">
-            <div class="restaurant-card-edit">
+            <div class="restaurant-card-edit" style="background-color: rgb(216, 213, 213); color: black;border-radius: 0 0 10px 10px;">
                 Update
                 <i class="far fa-edit"></i>
             </div>
